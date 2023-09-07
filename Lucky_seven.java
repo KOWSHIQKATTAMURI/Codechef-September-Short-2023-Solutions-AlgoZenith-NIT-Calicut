5ccc55a58b132f81b14bc2176676d50079d011f5
Kattamuri Kowshiq <kattamuri_b201298bt@nitc.ac.in>
2023-09-07 17:47:45 +0530

    Create Lucky_seven.java

---
+++ Lucky_seven.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef{
+	public static void main (String[] args) throws java.lang.Exception{
+		Scanner sc = new Scanner(System.in);
+		String S = sc.next();
+		System.out.println(S.charAt(6));
+	}
+}
